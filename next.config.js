c553ec9dfde8541d73d9d33aaafd99b06c045368
Alba Martínez <alba.martinez-diaz@capgemini.com>
2024-01-23 16:39:31 +0100

    Changing the configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
-}
+  assetPrefix: process.env.NODE_ENV === "production" ? "/calendar" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
