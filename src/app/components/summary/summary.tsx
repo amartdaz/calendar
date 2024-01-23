f489ed25e5caec8c4b41cb2dc03c5cd7e922ce11
Alba Martínez <alba.martinez-diaz@capgemini.com>
2024-01-23 16:05:53 +0100

    Some changes

--- src/app/components/summary/summary.tsx
+++ src/app/components/summary/summary.tsx
@@ -1,4 +1,12 @@
-export default function Summary({ holidays, ownBusiness, paidAbsence }: { holidays: number, ownBusiness: number, paidAbsence: number }) {
+export default function Summary({
+  holidays,
+  ownBusiness,
+  paidAbsence,
+}: {
+  holidays: number;
+  ownBusiness: number;
+  paidAbsence: number;
+}) {
   return (
     <div className="flex flex-wrap justify-around p-2 max-w-56 rounded mr-3 border border-solid border-current">
       <div>
@@ -6,16 +14,16 @@ export default function Summary({ holidays, ownBusiness, paidAbsence }: { holida
         <p className="festivity max-w-10 p-1">13</p>
       </div>
       <div>
-        <h1 className="max-w-24">Ausencias pagadas</h1>
-        <p className="paidAbsence max-w-10 p-1">{paidAbsence}</p>
+        <h1 className="max-w-24">Asuntos propios</h1>
+        <p className="ownBusiness max-w-10 p-1">{2 - ownBusiness}</p>
       </div>
       <div>
         <h1 className="max-w-24">Vacaciones</h1>
         <p className="holiday max-w-10 p-1">{24 - holidays}</p>
       </div>
       <div>
-        <h1 className="max-w-24">Asuntos propios</h1>
-        <p className="ownBusiness max-w-10 p-1">{2 - ownBusiness}</p>
+        <h1 className="max-w-24">Ausencias pagadas</h1>
+        <p className="paidAbsence max-w-10 p-1">{paidAbsence}</p>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import Month from "./components/month/month";
 import Summary from "./components/summary/summary";
+import Header from "./components/header/header";
 
 const months = [
   { name: "Enero", days: 31, init: 0 },
@@ -18,32 +19,54 @@ const months = [
   { name: "Diciembre", days: 31, init: 6 },
 ];
 export default function Home() {
-  const [dates, setDates] = useState('');
-  const [message, setMessage] = useState({text: 'Suuuuuuuuu', error: false});
+  const [dates, setDates] = useState("");
+  const [message, setMessage] = useState({ text: "Suuuuuuuuu", error: false });
 
   const onClick = () => {
-    if(dates.split(':h:').length <= 25) { 
-      localStorage.setItem('fechas', dates);
-      setMessage({text: 'Sigue así máquina', error: false});
-    } else {
-      setMessage({text: 'Has excedido el límite de vacaciones', error: true});
-    };
-  }
+    if (dates.split(":h:").length > 25) {
+      setMessage({ text: "Has excedido el límite de vacaciones", error: true });
+      return;
+    }
+    if (dates.split(":ob:").length > 3) {
+      setMessage({
+        text: "No creo que tengas tantos asuntos propios, revísatelo",
+        error: true,
+      });
+      return;
+    }
+    localStorage.setItem("fechas", dates);
+    setMessage({ text: "Sigue así máquina", error: false });
+  };
 
   useEffect(() => {
-    const local = localStorage.getItem('fechas');
-    if(local && !dates) {
+    const local = localStorage.getItem("fechas");
+    if (local && !dates) {
       setDates(local);
     }
-  }, [])
+  }, []);
 
   return (
     <main>
+      <Header />
       <div className="flex items-end m-3">
-        <Summary holidays={dates.split(':h:').length - 1} ownBusiness={dates.split(':ob:').length - 1} paidAbsence={dates.split(':pa:').length - 1} />
+        <Summary
+          holidays={dates.split(":h:").length - 1}
+          ownBusiness={dates.split(":ob:").length - 1}
+          paidAbsence={dates.split(":pa:").length - 1}
+        />
         <div className="ml-3">
-          <p className={message.error ? "error border-b border-solid border-current p-2 mb-3 min-w-96" : "normal border-b border-solid border-current p-2 mb-3 min-w-96"}>{message.text}</p>
-          <button className="save p-2 rounded" onClick={onClick}>Guardar</button>
+          <p
+            className={
+              message.error
+                ? "error border-b border-solid border-current p-2 mb-3 min-w-96"
+                : "normal border-b border-solid border-current p-2 mb-3 min-w-96"
+            }
+          >
+            {message.text}
+          </p>
+          <button className="save p-2 rounded" onClick={onClick}>
+            Guardar
+          </button>
         </div>
       </div>
       <div className="flex flex-wrap justify-around px-5">
